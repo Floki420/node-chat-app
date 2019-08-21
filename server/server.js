b29ef78702bb4f958bb51f2ecfb1aba736f9c6f3
floki420 <hasanjavedabbasi007@gmail.com>
2019-08-21 17:06:00 +0500

    New things added

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path=require('path');
 const express=require('express');
+const socketIO=require('socket.io');
+const http= require('http');
 
 const publicpath=path.join(__dirname , '../public');
 const port= process.env.PORT||3000;
 
 var app = express();
+var server=http.createServer(app);
+var io= socketIO(server);
+
+io.on('connection',(socket)=>{
+    console.log('A user is connected');
+    socket.on('disconnect',()=>{
+        console.log('User is disconneted');
+    });
+});
+
 
 app.use(express.static(publicpath));
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server on port ${port}`);
 });
